6cdaeaa80b53f45e360e8eb2f12809be62bde0ab
Arom Sanchez <arom.sanchez@tecsup.edu.pe>
2025-11-25 02:08:46 -0500

    fix: Move os import to top of settings.py for Redis configuration

--- conectaya/settings.py
+++ conectaya/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -207,8 +208,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-import os
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
